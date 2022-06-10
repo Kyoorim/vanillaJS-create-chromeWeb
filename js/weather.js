93b301732d61437de34ba414ef67390895abf4fa
Kyoorim <kyoorim88@gmail.com>
2022-06-10 13:42:24 +0900

    api key updated

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = " 82ee61288711c4393cd7dfff06564318";
+const API_KEY = "82ee61288711c4393cd7dfff06564318";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
